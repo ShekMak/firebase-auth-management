301ee83d6ce458bb8ccfa3773c76887ba2d5986e
ShekMak <majorsonshekinah@gmail.com>
2021-09-26 20:26:10 +0200

    last implementation in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,17 +8,33 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  subscription = new Subscription();
 
   constructor(private authService: AuthService,
               private router: Router) { }
 
   ngOnInit(): void {
+    this.subscription.add(
+      this.authService.getCurrentUser().subscribe(
+        userConnected => {
+          // In the connected user, you will find the metadata that
+          // will allow you to compare the creation date and the last connection date
+          console.log(userConnected)
+        }
+      )
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   signOut(): void {
     this.authService.signOut().then(
       () => {
+        this.subscription.unsubscribe();
         this.router.navigate(['login']);
       }
     ).catch(error => {
